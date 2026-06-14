4713480347d1d0d4d7614412554ae6dc8a03b6b1
Muhammad Ahmad Hamim <iamshahzadahmad650@gmail.com>
2026-06-15 00:41:38 +0500

    Polishing the palindrome algo code

--- C++/Algorithms/Math_algorithms/01_palindromeNumber.cpp
+++ C++/Algorithms/Math_algorithms/01_palindromeNumber.cpp
@@ -1,37 +1,37 @@
 /*Given an integer x, return true if x is a palindrome, and false otherwise. */
 #include<iostream>
 using namespace std;
-bool isPalindrome(int x) {
+bool isPalindrome(int x){
     
     bool is_p;
     
-    if(x<0){
-        is_p=false;
-    }        //Removing the negative numbers from the equation..
-    else{
-        int x2=x;      //Preserving the main value
-        int x1=0;     //Reversel 
+    if(x < 0){  // Removing the negative numbers from the equation..
+        is_p = false;
+        return is_p;
+    }        
+    
+    int x2 = x;      // Preserving the main value
+    int x1 = 0;     // Reversel 
         
-        while(true){
+    while(true){
             
-            if(x!=0){
-                int digit=x%10; //eeking out the last digit
+        if(x != 0){
+            int digit = x % 10; //eeking out the last digit
                 
-                x1=x1*10+digit;  //Making space for the incoming digit
+            x1= x1 * 10 + digit;  //Making space for the incoming digit
                 
-                x=x/10;    //Removing the used digit
-            }
-            else{
-                break;
-            }
-        }
-
-        if(x1==x2){
-            is_p=true;
+            x= x / 10;    //Removing the used digit
         }
         else{
-            is_p=false;
+            break;
         }
+    }
+
+    if(x1 == x2){
+        is_p = true;
+    }
+    else{
+        is_p = false;
     }
     
     return is_p;
